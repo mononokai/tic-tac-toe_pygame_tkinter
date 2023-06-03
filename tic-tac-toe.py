480367af048b711f24cc0d4e4bd2476a3a6d221a
mononokai <leachda12@icloud.com>
2023-06-02 22:15:44 -0400

    built display object in board class

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -7,3 +7,17 @@ class TicTacToeBoard(tk.Tk):
         self.title("Tic-Tac-Toe")
         # non-public dictionary for cells of the board
         self._cells = {}
+
+    def _create_board_display(self):
+        # create the frame for display
+        display_frame = tk.Frame(master=self)
+        # .pack() is a geometry manager being used to fill the frame to the window
+        display_frame.pack(fill=tk.X)
+        # the label object must be inside the frame object, which is why it references the display_frame itself as "master"
+        self.display = tk.Label(
+            master=display_frame,
+            text="Ready?",
+            font=font.Font(size=28, weight="bold"),
+        )
+        # packing the display label in the frame
+        self.display.pack()
