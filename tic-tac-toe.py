eb4f4b469b4e0a4f330d20dcf9216fa20889ea39
mononokai <leachda12@icloud.com>
2023-06-02 22:15:00 -0400

    started building out the board class

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,2 +1,9 @@
-import tkinter
-print(tkinter.TkVersion)
+import tkinter as tk
+from tkinter import font
+
+class TicTacToeBoard(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Tic-Tac-Toe")
+        # non-public dictionary for cells of the board
+        self._cells = {}
