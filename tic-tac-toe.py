f2c5ad8692fd30ae7d72a854da24d161d1544f99
mononokai <leachda12@icloud.com>
2023-06-02 23:17:55 -0400

    built grid object in board class

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -7,7 +7,7 @@ class TicTacToeBoard(tk.Tk):
         self.title("Tic-Tac-Toe")
         # non-public dictionary for cells of the board
         self._cells = {}
-
+    
     def _create_board_display(self):
         # create the frame for display
         display_frame = tk.Frame(master=self)
@@ -21,3 +21,35 @@ class TicTacToeBoard(tk.Tk):
         )
         # packing the display label in the frame
         self.display.pack()
+
+    def _create_board_grid(self):
+        # create a grid of the cells
+        grid_frame = tk.Frame(master=self)
+        # place the Frame object on the window
+        grid_frame.pack()
+        # loop from 0-2 to get row coordinates
+        for row in range(3):
+            # configure size of rows and columns
+            self.rowconfigure(row, weight=1, minsize=50)
+            self.columnconfigure(row, weight=1, minsize=75)
+            # loop through the column coordinates to create buttons for each cell in the grid
+            for col in range(3):
+                button = tk.Button(
+                    master=grid_frame,
+                    text="",
+                    font=font.Font(size=36, weight="bold"),
+                    fg="black",
+                    width=3,
+                    height=2,
+                    highlightbackground="lightblue",
+                )
+                # add the buttons to the ._cells dictionary with buttons being keys and row/col numbers being values
+                self._cells[button] = (row, col)
+                # add each button to the window with some styling
+                button.grid(
+                    row=row,
+                    column=col,
+                    padx=5,
+                    pady=5,
+                    sticky="nsew",
+                )
