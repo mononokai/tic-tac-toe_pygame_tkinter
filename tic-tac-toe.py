37095a85e330eae20c0dbb21212b4e40d336862e
mononokai <leachd12@icloud.com>
2023-06-19 12:28:17 -0400

    fix: indentation issue in TicTacToeBoard

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -81,38 +81,38 @@ class TicTacToeBoard(tk.Tk):
                     pady=5,
                     sticky="nsew",
                 )
-        def play(self, event):
-            clicked_button = event.widget
-            row, col = self._cells[clicked_button]
-            move = Move(row, col, self._game.current_player.label)
-            if self._game.is_valid_move(move):
-                self._update_button(clicked_button)
-                self._game.process_move(move)
-                if self._game.is_tied():
-                    self._update_display(msg="Tie game!", color="red")
-                elif self._game.has_winner():
-                    self._highlight_cells()
-                    msd = f"{self._game.current_player.label} won!"
-                    color = self._game.current_player.color
-                    self._update_display(msg, color)
-                else:
-                    self._game.toggle_player
-                    msg = f"{self._game.current_player.label}'s turn"
-                    self._update_display(msg)
-            
-            def _update_button(self, clicked_button):
-                clicked_button.config(text=self._game.current_player.label)
-                clicked_button.config(fg=self._game.current_player.color)
+    def play(self, event):
+        clicked_button = event.widget
+        row, col = self._cells[clicked_button]
+        move = Move(row, col, self._game.current_player.label)
+        if self._game.is_valid_move(move):
+            self._update_button(clicked_button)
+            self._game.process_move(move)
+            if self._game.is_tied():
+                self._update_display(msg="Tie game!", color="red")
+            elif self._game.has_winner():
+                self._highlight_cells()
+                msd = f"{self._game.current_player.label} won!"
+                color = self._game.current_player.color
+                self._update_display(msg, color)
+            else:
+                self._game.toggle_player
+                msg = f"{self._game.current_player.label}'s turn"
+                self._update_display(msg)
+        
+        def _update_button(self, clicked_button):
+            clicked_button.config(text=self._game.current_player.label)
+            clicked_button.config(fg=self._game.current_player.color)
 
-            def _update_display(self, msg, color="black"):
-                self.display["test"] = msg
-                self.display["fg"] = color
-            
-            def _highlight_cells(self):
-                for button, coordinates in self._cells.items():
-                    row, col = coordinates
-                    if (row, col) in self._game.winner_combo:
-                        button.config(hightlightbackground="red")
+        def _update_display(self, msg, color="black"):
+            self.display["test"] = msg
+            self.display["fg"] = color
+        
+        def _highlight_cells(self):
+            for button, coordinates in self._cells.items():
+                row, col = coordinates
+                if (row, col) in self._game.winner_combo:
+                    button.config(hightlightbackground="red")
 
 
 class TicTacToeGame:
@@ -136,7 +136,7 @@ class TicTacToeGame:
     def _get_winning_combos(self):
         rows = [
             [(move.row, move.col) for move in row]
-            for row in self.__current_moves
+            for row in self._current_moves
         ]
         columns = [list(col) for col in zip(*rows)]
         first_diagonal = [row[i] for i, row in enumerate(rows)]
@@ -146,16 +146,16 @@ class TicTacToeGame:
     def is_valid_move(self, move):
         row = move.row
         col = move.col
-        move_was_not_played = self.__current_moves[row][col].label == ""
-        no_winner = not self.__has_winner
+        move_was_not_played = self._current_moves[row][col].label == ""
+        no_winner = not self._has_winner
         return no_winner and move_was_not_played
         
     def process_move(self, move):
         row, col = move.row, move.col
-        self.__current_moves[row][col] = move
+        self._current_moves[row][col] = move
         for combo in self._winning_combos:
             results = set(
-                self.__current_moves[n][m].label
+                self._current_moves[n][m].label
                 for n, m in combo
             )
             is_win = (len(results) == 1) and ("" not in results)
@@ -179,7 +179,8 @@ class TicTacToeGame:
 
 
 def main():
-    board = TicTacToeBoard()
+    game = TicTacToeGame()
+    board = TicTacToeBoard(game)
     board.mainloop()
 
 
